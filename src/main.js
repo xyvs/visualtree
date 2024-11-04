67e753fabf18fb0c231ec3bfe410b2e1a3234a87
xyvs <xyvs@users.noreply.github.com>
2024-11-04 21:03:28 +0000

    style: format files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./index.css";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
